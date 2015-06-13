8d8451e380b048722cd5fa2a7f768a5c9687ad34
imaginationcoder <labs@maisasolutions.com>
2015-06-13 11:11:20 +0530

    removed categories model

--- app/models/category.rb
+++
@@ -1,7 +0,0 @@
-class Category
-  include Mongoid::Document
-  include Mongoid::Timestamps
-  include Mongoid::Attributes::Dynamic
-
-  has_many :items, :class_name => 'Products::Item', foreign_key: :category_id
-end
