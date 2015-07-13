a6926856a2dd8476f9eed81435c4917732c5e92b
imaginationcoder <labs@maisasolutions.com>
2015-07-13 14:26:34 +0530

    new way of intiating projects

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,9 +7,21 @@ class Item
 
   embedded_in :project
 
-  def new_product=(arg)
-    product = eval(type).create(arg)
-
+  def product=(arg)
+    if arg.include?('new')
+      product = eval(type).new(arg)
+      product.assign_attributes(arg['existing'])
+    else
+      price = Price.find_by(artline_item_number:arg['existing']['number'] )
+      org_prod = price.product if price and price.product
+      product = eval(type).where(arg['existing'].reject{|k,v| k.eql?("number")}).find_or_initialize_by.tap do |pro|
+        org_prod.field_names.each do |pfield|
+          pro[pfield] = org_prod[pfield]
+        end if org_prod
+      end
+      self["number"] = arg['existing']['number'] if !product.new_record? and product.eql?(org_prod)
+    end
+    product.save
     unless product.errors.any?
       self["product_id"] = product.id
     else
@@ -17,41 +29,51 @@ class Item
     end
   end
 
-  def new_shade=(arg)
-    product= Shade.create(arg)
-    unless product.errors.any?
-      self["shade_id"] = product.id
-    else
-      errors.add(:new_shade, product.errors.full_messages.join(', '))
-    end
-  end
+  # def new_product=(arg)
+  #   product = eval(type).create(arg)
+  #
+  #   unless product.errors.any?
+  #     self["product_id"] = product.id
+  #   else
+  #     errors.add(:new_item, product.errors.full_messages.join(', '))
+  #   end
+  # end
 
-  def new_bulb=(arg)
-    product = Bulb.create(arg)
-    unless product.errors.any?
-      self["bulb_id"] = product.id
-    else
-      errors.add(:new_shade, product.errors.full_messages.join(', '))
-    end
-  end
-
-  def new_moulding_company=(arg)
-    moulding_company = MouldingCompany.create(arg)
-    unless moulding_company.errors.any?
-      self["moulding_company_id"] = moulding_company.id
-    else
-      errors.add(:name, moulding_company.errors.full_messages.join(', '))
-    end
-  end
-
-  def new_frame=(arg)
-    frame = Frame.create(arg)
-    unless frame.errors.any?
-      self["frame_id"] = frame.id
-    else
-      errors.add(:name, frame.errors.full_messages.join(', '))
-    end
-  end
+  # def new_shade=(arg)
+  #   product= Shade.create(arg)
+  #   unless product.errors.any?
+  #     self["shade_id"] = product.id
+  #   else
+  #     errors.add(:new_shade, product.errors.full_messages.join(', '))
+  #   end
+  # end
+  #
+  # def new_bulb=(arg)
+  #   product = Bulb.create(arg)
+  #   unless product.errors.any?
+  #     self["bulb_id"] = product.id
+  #   else
+  #     errors.add(:new_shade, product.errors.full_messages.join(', '))
+  #   end
+  # end
+  #
+  # def new_moulding_company=(arg)
+  #   moulding_company = MouldingCompany.create(arg)
+  #   unless moulding_company.errors.any?
+  #     self["moulding_company_id"] = moulding_company.id
+  #   else
+  #     errors.add(:name, moulding_company.errors.full_messages.join(', '))
+  #   end
+  # end
+  #
+  # def new_frame=(arg)
+  #   frame = Frame.create(arg)
+  #   unless frame.errors.any?
+  #     self["frame_id"] = frame.id
+  #   else
+  #     errors.add(:name, frame.errors.full_messages.join(', '))
+  #   end
+  # end
 
   def supplier_cost
     item_price.supplier_cost
--- app/models/products/lamp.rb
+++ app/models/products/lamp.rb
@@ -1,9 +1,17 @@
 class Products::Lamp < Product
 
+
   field :description
   field :size
+  field :shade_id
+  field :bulb_id
 
   #need image source field source
   mount_uploader :source, SourceUploader
+
+  def field_names
+    %w(description size)
+  end
+
 
 end
